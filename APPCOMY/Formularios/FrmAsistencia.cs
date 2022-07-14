e948c2566a89713ac4988a5744da619bce984672
2022yoreko <katherinesofia719@gmail.com>
2022-07-13 21:16:08 -0600

    Cambios 9550

--- APPCOMY/Formularios/FrmAsistencia.cs
+++ APPCOMY/Formularios/FrmAsistencia.cs
@@ -41,16 +41,22 @@ namespace APPCOMY
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string Nombre = txtNombre.Text;
+
+            string Lu = checkBox1.Checked.ToString();
+            string Ma = checkBox2.Checked.ToString();
+            string Mi = checkBox2.Checked.ToString();
+            string Ju = checkBox2.Checked.ToString();
+            string Vi = checkBox2.Checked.ToString();
+            string Sa = checkBox2.Checked.ToString();
+            string Do = checkBox2.Checked.ToString();
             string Fecha = txtFecha.Text;
             string Hora1 = txtHora1.Text;
             string Hora2 = txtHora2.Text;
             string Hora3 = txtHora3.Text;
-           
-            bool Lu, Ma, Mi, Ju, Vi, Sa, Do;
 
-            bool Guardar = true;
+            bool validate = validateData(Lu, Ma, Mi, Ju, Vi, Sa, Do);
 
+            if (!validate) return;
 
             FileStream fs;
             StreamWriter escribe;
@@ -61,132 +67,52 @@ namespace APPCOMY
             fs = new FileStream(rutarchivo, FileMode.Append);
             escribe = new StreamWriter(fs);
 
-            Lu = Convert.ToBoolean(Console.ReadLine());
-            Ma = Convert.ToBoolean(Console.ReadLine());
-            Mi = Convert.ToBoolean(Console.ReadLine());
-            Ju = Convert.ToBoolean(Console.ReadLine());
-            Vi = Convert.ToBoolean(Console.ReadLine());
-            Sa = Convert.ToBoolean(Console.ReadLine());
-            Do = Convert.ToBoolean(Console.ReadLine());
-
-
-            if (Lu == true)
+            try
             {
-                Lu = checkBox1.Checked;
+                linea = txtNombre.Text + ";";
+                linea += checkBox1.Checked.ToString() + ";";
+                linea += checkBox2.Checked.ToString() + ";";
+                linea += checkBox3.Checked.ToString() + ";";
+                linea += checkBox4.Checked.ToString() + ";";
+                linea += checkBox5.Checked.ToString() + ";";
+                linea += checkBox6.Checked.ToString() + ";";
+                linea += checkBox7.Checked.ToString() + ";";
+                linea += checkBox8.Checked.ToString() + ";";
+                linea += checkBox9.Checked.ToString() + ";";
+                linea += checkBox10.Checked.ToString() + ";";
+                linea += checkBox11.Checked.ToString() + ";";
+                linea += checkBox12.Checked.ToString() + ";";
+                linea += checkBox13.Checked.ToString() + ";";
+                linea += checkBox14.Checked.ToString() + ";";
+                linea += checkBox15.Checked.ToString() + ";";
+                linea += checkBox16.Checked.ToString() + ";";
+                linea += checkBox17.Checked.ToString() + ";";
+                linea += checkBox18.Checked.ToString() + ";";
+                linea += checkBox19.Checked.ToString() + ";";
+                linea += checkBox20.Checked.ToString() + ";";
+                linea += checkBox21.Checked.ToString() + ";";
+                linea += txtFecha.Text + ";";
+                linea += txtHora1.Text + ";";
+                linea += txtHora2.Text + ";";
+                linea += txtHora3.Text + ";";
+                escribe.WriteLine(linea.ToUpper());
+                escribe.Close();
             }
-            else if (Lu == true)
+            catch 
             {
-                Lu = checkBox2.Checked;
-            }
-            else if (Lu == true)
-            {
-                Lu = checkBox3.Checked;
-            }
-
-
-
-            if (Ma == true)
-            {
-                Ma = checkBox4.Checked;
-            }
-            else if (Ma == true)
-            {
-                Ma = checkBox5.Checked;
-            }
-            else if (Ma == true)
-            {
-                Ma = checkBox6.Checked;
-            }
-
-
-
-            if (Mi == true)
-            {
-                Mi = checkBox7.Checked;
-            }
-            else if (Mi == true)
-            {
-                Mi = checkBox8.Checked;
-            }
-            else if (Mi == true)
-            {
-                Mi = checkBox9.Checked;
+                MessageBox.Show("Debe marcar todos los datos");
             }
 
-
-
-            if (Ju == true)
-            {
-                Ju = checkBox10.Checked;
-            }
-            else if (Ju == true)
-            {
-                Ju = checkBox11.Checked;
-            }
-            else if (Ju == true)
-            {
-                Ju = checkBox12.Checked;
-            }
-
-
-
-            if (Vi == true)
-            {
-                Vi = checkBox13.Checked;
-            }
-            else if (Vi == true)
-            {
-                Vi = checkBox14.Checked;
-            }
-            else if (Vi == true)
-            {
-                Vi = checkBox15.Checked;
-            }
-
-
-
-            if (Sa == true)
-            {
-                Sa = checkBox16.Checked;
-            }
-            else if (Sa == true)
-            {
-                Sa = checkBox17.Checked;
-            }
-            else if (Sa == true)
-            {
-                Sa = checkBox18.Checked;
-            }
-
-
-
-            if (Do == true)
-            {
-                Do = checkBox19.Checked;
-            }
-            else if (Do == true)
-            {
-                Do = checkBox20.Checked;
-            }
-            else if (Do == true)
-            {
-                Do = checkBox21.Checked;
-            }
-
-            
-            linea = txtNombre.Text + ";";
-            linea = txtFecha.Text + ";";
-            linea += txtHora1.Text + ";";
-            linea += txtHora2.Text + ";";
-            linea += txtHora3.Text + ";";
-
-            
-              /*  MessageBox.Show("Debe marcar todos los datos");*/
-            if (Guardar == true) 
+            if (validate == true) 
             {
                 MessageBox.Show("Asistencia Guardada");
             }
 
+        }
+
+        private bool validateData(string lu, string ma, string mi, string ju, string vi, string sa, string @do)
+        {
+            throw new NotImplementedException();
         }
     }
 }
